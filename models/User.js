55ecfe99db89b6e1614aecc3107a5da395c6d62d
YggdrasilJL <yggdrasiljl@gmail.com>
2023-08-01 10:39:52 -0300

    add boilerplate code to get started

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    
+}
+
+User.init(
+    {
+
+    }
+)
+
+
+module.exports = User
