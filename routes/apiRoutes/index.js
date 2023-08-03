e2f66ea87bcac5007a74ce0ebb5b34d417fdcc14
YggdrasilJL <yggdrasiljl@gmail.com>
2023-08-03 16:27:24 -0300

    further routing for html

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const mapRoutes = require('./mapRoutes');
+const userRoutes = require('./userRoutes');
 
+router.use('/map', mapRoutes)
+router.use('/user', userRoutes)
 
-module.exports = router
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,33 @@
 const router = require('express').Router();
-const htmlRoutes = require('./htmlRoutes')
+const path = require('path');
+const htmlRoutes = require('./htmlRoutes');
 const apiRoutes = require('./apiRoutes');
 
-router.use('/', htmlRoutes)
-router.use('/api', apiRoutes)
+router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
-// send 404 path here '*'
+// the root url
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
+// mapbuilder page
+router.get('/map', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/mapBuilder.html'));
+});
+
+// login page
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/login.html'));
+});
+
+// faq page
+router.get('/faq', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/faq.html'));
+});
+
+// send random strings to 404 path
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/404.html'));
+});
 module.exports = router;
