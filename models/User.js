92073a09b4760114649486493eb01a7bec96b830
YggdrasilJL <yggdrasiljl@gmail.com>
2023-08-02 22:25:02 -0300

    boilerplate for routes and add app.get for server.js

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { v4: uuid } = require('uuid');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'users',
+  }
+);
+
+module.exports = User;
