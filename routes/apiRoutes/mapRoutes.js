8f5ef1c0524301cb83f99f758905faf3081bf693
nikkivno <nikkivigneault@gmail.com>
2023-08-05 17:40:34 -0300

    end of corrections maybe?

--- routes/apiRoutes/mapRoutes.js
+++ routes/apiRoutes/mapRoutes.js
@@ -35,11 +35,11 @@ router.get('/:id', async (req, res) => {
 
   router.put('/:userId/maps/:mapId', async (req, res) => {
     try {
-      const { userId, mapId } = req.params;
-      const mapData = req.body.mapData; 
+      const {userId, mapId} = req.params;
+      const {mapData} = req.body.mapData; 
 
       const map = await UserCreations.findByPk(mapId);
-      if (!map || map.user.Id !== userId) {
+      if (!map || map.user_name !== userId) {
         return res.status(404).json({error: 'Map not found.'});
       }
       map.mapData = mapData;
@@ -55,7 +55,7 @@ router.get('/:id', async (req, res) => {
     try {
       const { userId, mapId } = req.params;
       // Find the map and delete it
-      await User.destroy({ where: { userId, id: mapId } });
+      await UserCreations.destroy({ where: { id: mapId, user_name: userId } });
       res.json({ message: 'Map deleted successfully.' });
     } catch (err) {
       res.status(500).json({ error: 'Failed to delete the map.' });
