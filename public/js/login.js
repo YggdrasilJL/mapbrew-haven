7c9e5e85e1ef83b1f398a3d89fc0fa43e9a78c7c
nikkivno <nikkivigneault@gmail.com>
2023-08-07 18:48:29 -0300

    lock/unlock functioning in sign up password inputs

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,40 @@
 // Login password Hide/show
 
 let lockicon = document.getElementById('lockicon');
-let password = document.querySelector('#password');
+let newlockicon = document.getElementById('newlockicon');
+let lockicon2 = document.getElementById('lockicon2');
+
+let password = document.getElementById('password');
+let newpassword = document.getElementById('newPassword');
+let confirmpassword = document.getElementById('confirmPassword');
 
 lockicon.onclick = function() {
     if(password.type == 'password'){
         password.type = 'text';
         lockicon.src= '../assets/images/unlock.png';
     } else {
-        password.type = 'password'
-        lockicon.src = '../assets/images/lock.png'
+        password.type = 'password';
+        lockicon.src = '../assets/images/lock.png';
+    }
+};
+
+newlockicon.onclick = function() {
+    if(newpassword.type == 'password'){
+        newpassword.type = 'text';
+        newlockicon.src= '../assets/images/unlock.png';
+    } else {
+        newpassword.type = 'password';
+        newlockicon.src = '../assets/images/lock.png';
+    }
+};
+
+lockicon2.onclick = function() {
+    if(confirmpassword.type == 'password'){
+        confirmpassword.type = 'text';
+        lockicon2.src= '../assets/images/unlock.png';
+    } else {
+        confirmpassword.type = 'password';
+        lockicon2.src = '../assets/images/lock.png';
     }
 };
 
