4c6e9d49c0ce3da5c55fb9d26e39bd66a7cef261
YggdrasilJL <yggdrasiljl@gmail.com>
2023-08-08 16:13:24 -0300

    fix the show/hide btn only toggling each of the btns

--- public/js/faq.js
+++ public/js/faq.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.querySelector('.collapseAll');
   const btnText = document.querySelector('.all');
-
+  const colContain = document.querySelectorAll('.contain');
   let isToggled = false;
 
-  toggleBtn.addEventListener('click', () => {
-    isToggled
-      ? (btnText.textContent = 'Show all answers')
-      : (btnText.textContent = 'Hide all answers');
-    isToggled = !isToggled;
-  });
+  function toggleAns() {
+    toggleBtn.addEventListener('click', () => {
+      isToggled = !isToggled;
+
+      if (isToggled) {
+        for (const container of colContain) {
+          if (!container.classList.contains('show')) {
+            const collapseElement = new bootstrap.Collapse(container);
+            collapseElement.show();
+          }
+        }
+        btnText.textContent = 'Hide all answers';
+      } else {
+        for (const container of colContain) {
+          if (container.classList.contains('show')) {
+            const collapseElement = new bootstrap.Collapse(container);
+            collapseElement.hide();
+          }
+        }
+        btnText.textContent = 'Show all answers';
+      }
+    });
+  }
+
+  toggleAns();
 });
