e2208ff2bda13be3bc924328932854cbf379af7f
YggdrasilJL <yggdrasiljl@gmail.com>
2023-08-11 18:43:54 -0300

    fix some issues

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
+const express = require('express');
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const path = require('path');
-const express = require('express');
-const session = require('express-session');
 const routes = require('./controllers');
 const methodOverride = require('method-override');
 
-const users = [];
-const bcrypt = require('bcrypt');
-const passport = require('passport');
 const flash = require('express-flash');
-
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport,
-  (email) => users.find((user) => user.email === email),
-  (id) => users.find((user) => user.id === id)
-);
 
 const app = express();
 const sess = {
   secret: 'secret here',
-  cookie: {},
+  cookie: {
+    maxAge: 1800000,
+    name: 'myServerID',
+  },
   resave: false,
+  saveUninitialized: true,
   store: new sequelizeStore({
     db: sequelize,
   }),
@@ -38,11 +32,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view-engine', 'ejs');
 app.use(flash());
 app.use(methodOverride('_method'));
-app.use(passport.initialize());
-app.use(passport.session());
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name });
-});
+// app.get('/', checkAuthenticated, (req, res) => {
+//   res.render('index.ejs', { name: req.user.name });
+// });
 
 app.use(routes);
 
@@ -50,59 +42,59 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-app.get('/login', checknotAuthenticated, (req, res) => {
-  res.render('login.ejs');
-});
+// app.get('/login', checknotAuthenticated, (req, res) => {
+//   res.render('login.ejs');
+// });
 
-app.get('/register', checknotAuthenticated, (req, res) => {
-  res.render('register.ejs');
-});
+// app.get('/register', checknotAuthenticated, (req, res) => {
+//   res.render('register.ejs');
+// });
 
-app.post(
-  '/login',
-  checknotAuthenticated,
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureflash: true,
-  })
-);
+// app.post(
+//   '/login',
+//   checknotAuthenticated,
+//   passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureRedirect: '/login',
+//     failureflash: true,
+//   })
+// );
 
-app.post('/register', checknotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-    res.redirect('/login');
-  } catch {
-    res.redirect('/register');
-  }
-  console.log(users);
-});
+// app.post('/register', checknotAuthenticated, async (req, res) => {
+//   try {
+//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//     users.push({
+//       id: Date.now().toString(),
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: hashedPassword,
+//     });
+//     res.redirect('/login');
+//   } catch {
+//     res.redirect('/register');
+//   }
+//   console.log(users);
+// });
 
-app.delete('/logout', (req, res) => {
-  req.logOut();
-  res.redirect('/login');
-});
+// app.delete('/logout', (req, res) => {
+//   req.logOut();
+//   res.redirect('/login');
+// });
 
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
+// function checkAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return next();
+//   }
+//   res.redirect('/login');
+// }
 
-function checknotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  next();
-}
+// function checknotAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return res.redirect('/');
+//   }
+//   next();
+// }
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}.`));
 });
