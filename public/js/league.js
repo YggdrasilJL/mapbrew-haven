3845efa4fff344f2970ff328d997f29e80da9503
nikkivno <nikkivigneault@gmail.com>
2023-08-11 15:44:01 -0300

    Doctype added to league.html, as well as some typo corrections

--- public/js/league.js
+++ public/js/league.js
@@ -1,43 +1,43 @@
-const userNameInput = document.getElementById('username');
-const userCreationsInput = document.getElementById('usercreations');
-const reviewInput = document.getElementById('review');
-const reviewForm = document.getElementById('review-form');
+// const userNameInput = document.getElementById('username');
+// const userCreationsInput = document.getElementById('usercreations');
+// const reviewInput = document.getElementById('review');
+// const reviewForm = document.getElementById('review-form');
 
-// Helper function that accepts a `review` object, sends a POST request and returns the result
-const postReview = (review) =>
-    // Fetch accepts a URL and an options object where you can declare the HTTP method, the request body, and any headers.
-    fetch('api/reviews', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(review),
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log('Successful POST request:', data);
-            return data;
-        })
-        .catch((error) => {
-            console.error('Error in POST request:', error);
-        });
+// // Helper function that accepts a `review` object, sends a POST request and returns the result
+// const postReview = (review) =>
+//     // Fetch accepts a URL and an options object where you can declare the HTTP method, the request body, and any headers.
+//     fetch('api/reviews', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(review),
+//     })
+//         .then((res) => res.json())
+//         .then((data) => {
+//             console.log('Successful POST request:', data);
+//             return data;
+//         })
+//         .catch((error) => {
+//             console.error('Error in POST request:', error);
+//         });
 
-// Listen for when the form is submitted
-reviewForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+// // Listen for when the form is submitted
+// reviewForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    // Create a new review object from the input values
-    const newReview = {
-        userName: userNameInput.value.trim(),
-        userCreations: userCreationsInput.value.trim(),
-        review: reviewInput.value.trim(),
-    };
+//     // Create a new review object from the input values
+//     const newReview = {
+//         userName: userNameInput.value.trim(),
+//         userCreations: userCreationsInput.value.trim(),
+//         review: reviewInput.value.trim(),
+//     };
 
-    // Call our postReview method to make a POST request with our `newReview` object.
-    postReview(newReview)
-        .then((data) => alert(`Review added! Review ID: ${data.body.review_id}`))
-        .catch((err) => console.error(err));
-});
+//     // Call our postReview method to make a POST request with our `newReview` object.
+//     postReview(newReview)
+//         .then((data) => alert(`Review added! Review ID: ${data.body.review_id}`))
+//         .catch((err) => console.error(err));
+// });
 
 // let swiper = new Swiper(".mySwiper", {
 //     effect: "coverflow",
@@ -57,5 +57,9 @@ reviewForm.addEventListener('submit', (e) => {
 // });
 
 new Swiper('#swiper-1', {
-    effect: 'fade'
+    effect: 'fade',
+    pagination: {
+        el: '#swiper-1 .swiperpagination',
+        clickable: true
+    }
 });
