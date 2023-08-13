1e1e0d00f6ded8cbf627e166a61724d8ace562c5
Scoges <scott.ogrins@gmail.com>
2023-08-13 13:09:52 -0300

    changed ejs to homeroutes

--- controllers/htmlRoutes/homeRoutes.js
+++ controllers/htmlRoutes/homeRoutes.js
@@ -7,46 +7,59 @@ const app = express();
 app.set('view-engine', 'ejs')
 
  //login page
-router.get('/', withauth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+//outer.get('/', withauth, async (req, res) => {
+  //try {
+   // const userData = await User.findAll({
+   //   attributes: { exclude: ['password'] },
+   //   order: [['name', 'ASC']],
+   // });
 
-    const users = userData.map((project) => project.get({ plain: true }))
+  //  const users = userData.map((project) => project.get({ plain: true }))
 
     
-    res.render('homepage', {
-      users,
-     logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-   res.status(500).json(err);
-  }
+   // res.render('homepage', {
+   //   users,
+    // logged_in: req.session.logged_in,
+   // });
+ // } catch (err) {
+ //  res.status(500).json(err);
+  //}
+//});
+
+//router.get('/login', (req, res) => {
+ // if (req.session.logged_in) {
+ // res.redirect('/');
+ //   return;
+ // }
+
+ // res.render('login');
+//});
+
+router.get('/', (req, res) => {
+  res.render('index.ejs');
+ });
+ 
+
+router.get('/index', (req, res) => {
+ res.render('index.ejs');
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-  res.redirect('/');
-    return;
-  }
 
-  res.render('login');
-});
+router.get("/mapbuilder",(req, res)=> {
+ res.render("mapbuilder.ejs")
+})
 
-router.get('/faq', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/faq.html'));
-});
+router.get("/league",(req, res)=> {
+ res.render("league.ejs")
+})
 
-// send random strings to 404 path
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/404.html'));
-});
+router.get("/login",(req, res)=> {
+ res.render("login.ejs")
+})
 
-// mapbuilder page
-router.get('/map', (req, res) => {
-  res.sendFile(path.join(__dirname, '.././public/html/mapBuilder.html'));
-});
+
+router.get("/faq",(req, res)=> {
+ res.render("faq.ejs")
+})
 
 module.exports = router;
