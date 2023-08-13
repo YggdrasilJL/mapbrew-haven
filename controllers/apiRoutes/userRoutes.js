599621a737b49704ec4ef9a657f0c07932670dfc
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-08-13 18:13:26 -0300

    changes to routes

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -1,40 +1,71 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+// const { v4: uuid } = require('uuid');
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+
+// get all users
+router.get('/', async (req, res) => {
+  const allUsers = await User.findAll();
+  res.json(allUsers);
+});
+// get user by id
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  res.json(user);
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { user_name, email, password } = req.body;
+    const hashedPw = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      id: uuid(),
+      user_name,
+      email,
+      password: hashedPw,
+    });
+
+    res.status(201).json({ message: 'User registered.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error registering User.' });
+  }
+});
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const { user_name, password } = req.body;
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: 'You are now logged in!' });
+    const user = await User.findOne({
+      where: { user_name },
     });
 
+    if (!user) {
+      res.status(400).json({ message: 'Invalid Credentials' });
+      return;
+    }
+
+    const validPw = await user.checkPassword(password);
+
+    if (!validPw) {
+      res.status(400).json({ message: 'Invalid Credentials' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json({ user: user, message: "You're now logged in." });
+    });
   } catch (err) {
-    res.status(400).json(err);
+    console.error(err);
+    res.status(500).json({ message: 'Error logging in' });
+    res.redirect('/login');
   }
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
